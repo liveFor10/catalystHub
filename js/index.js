47bfae95bb5e753d5250582c8070002390c2b738
Don Sutton <liveFor10@yahoo.com>
2022-05-05 21:23:08 -0700

    eod 5/5, viewport, css type, index.js, global margin reset

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById("gtRegister").onclick = function () { location.href = "register.html"; };
+document.getElementById("gtSignIn").onclick = function () { location.href = "signin.html"; };
+//document.getElementById("gtEmployers").onclick = function () { location.href = "employers.html"; };
+document.getElementById("gtEmployers").onclick = function () { location.href = "https://docs.google.com/forms/d/13GbUR0TM1onqjopi6fW6hHxi_IIIobOGOqV7sFbHQXM/edit?ts=626b3c8c"; };
+//document.getElementById("gtSearchJobs").onclick = function () { location.href = "jobSearch.html"; }
+document.getElementById("gtSearchJobs").onclick = function () { location.href = "https://sites.google.com/view/pocshelli"; };
+document.getElementById("gtContact").onclick = function () { location.href = "contact.html"; };
+document.getElementById("gtAbout").onclick = function () { location.href = "about.html"; };
